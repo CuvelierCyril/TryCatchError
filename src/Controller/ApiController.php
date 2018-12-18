1f37d548ff82caeb2b01051afadb0502313b2356
CyrilC <cyrilcuvelier@sfr.fr>
2018-12-18 15:08:02 +0100

    add both controllers from the protoype and connexion template

---
+++ src/Controller/ApiController.php
@@ -0,0 +1,291 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\User;
+use App\Entity\Subject;
+use App\Entity\Answer;
+use \DateTime;
+
+/**
+ * @route("/api/")
+ */
+class ApiController extends AbstractController{
+
+    /**
+     * @route("register/", name="apiRegister", methods="POST")
+     */
+    public function apiRegister(Request $request){
+        if ($request->getMethod() == 'POST'){
+            $email = $request->request->get('email');
+            $firstname = $request->request->get('firstname');
+            $lastname = $request->request->get('lastname');
+            $nickname = $request->request->get('nickname');
+            $password = $request->request->get('password');
+            $passwordConfirm = $request->request->get('passwordConfirm');
+
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
+                $msg['email'] = true;
+            }
+            if (!preg_match('#^[a-zA-Z\'\- ]{3,50}$#', $firstname)){
+                $msg['firstname'] = true;
+            }
+            if (!preg_match('#^[a-zA-Z\'\- ]{3,50}$#', $lastname)){
+                $msg['lastname'] = true;
+            }
+            if (!preg_match('#^[a-zA-Z0-9áàâäãåçéèêëíìîïñóòôöõúùûüýÿæœÁÀÂÄÃÅÇÉÈÊËÍÌÎÏÑÓÒÔÖÕÚÙÛÜÝŸÆŒ._-]{3,50}$#', $nickname)){
+                $msg['nickname'] = true;
+            }
+            if (!preg_match('#^.{3,100}$#', $password)){
+                $msg['password'] = true;
+            }
+            if ($password != $passwordConfirm){
+                $msg['passwordConfirm'] = true;
+            }
+
+            if (!isset($msg)){
+                $repo = $this->getDoctrine()->getRepository(User::class);
+                $user = $repo->findOneByEmail($email);
+                if ($user == null){
+                    $user = $repo->findOneByNickname($nickname);
+                    if ($user == null){
+                        $token = md5(rand().uniqid().time());
+                        $newUser = new User();
+                        $newUser
+                            ->setEmail($email)
+                            ->setPassword(password_hash($password, PASSWORD_BCRYPT))
+                            ->setFirstname($firstname)
+                            ->setLastname($lastname)
+                            ->setNickname($nickname)
+                            ->setScore(0)
+                            ->setAccess(1)
+                            ->setToken($token)
+                            ->setValidate(0)
+                            ->setimage('img/default.png')
+                        ;
+                        $em = $this->getDoctrine()->getManager();
+                        $em->persist($newUser);
+                        $em->flush();
+                        $msg['success'] = true;
+                    } else {
+                        $msg['nicknameExists'] = true;
+                    }
+                } else {
+                    $msg['emailExists'] = true;
+                }
+            }
+        }
+        return $this->json($msg);
+    }
+
+    /**
+     * @route("login/", name="apiLogin", methods="POST")
+     */
+    public function apiLogin(Request $request){
+        if ($request->getMethod() == 'POST'){
+            $email = $request->request->get('email');
+            $password = $request->request->get('password');
+
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)){
+                $msg['email'] = true;
+            }
+            if (!preg_match('#^.{3,100}$#', $password)){
+                $msg['password'] = true;
+            }
+            if (!isset($msg)){
+                $repo = $this->getDoctrine()->getRepository(User::class);
+                $user = $repo->findOneByEmail($email);
+                if ($user != null){
+                    if (password_verify($password, $user->getPassword())){
+                        $this->get('session')->set('account', $user);
+                        $msg['success'] = true;
+                    } else {
+                        $msg['passwordInvalid'] = true;
+                    }
+                } else {
+                    $msg['emailDoesntExist'] = true;
+                }
+            }
+        }
+        return $this->json($msg);
+    }
+
+    /**
+     * @route("create-subject/", name="apiCreateSubject", methods="POST")
+     */
+    public function apiCreateSubject(Request $request){
+        if ($request->getMethod() == 'POST'){
+            $title = $request->request->get('title');
+            $content = $request->request->get('content');
+
+            if (!preg_match('#^[a-zA-Z0-9 \'\-_ !,.?:/]{5,100}$#', $title)){
+                $msg['title'] = true;
+            }
+            if (!preg_match('#^[a-zA-Z0-9 \'\-_ !,.?:/]{5,10000}$#', $content)){
+                $msg['content'] = true;
+            }
+
+            if (!isset($msg)){
+                $newSubject = new Subject();
+                $newSubject
+                    ->setTitle($title)
+                    ->setContent($content)
+                    ->setView(0)
+                    ->setDate(new DateTime())
+                    ->setAuthor($this->get('session')->get('account'))
+                ;
+                $em = $this->getDoctrine()->getManager();
+                $em->merge($newSubject);
+                $em->flush();
+                $msg['success'] = true;
+            }
+        }
+        return $this->json($msg);
+    }
+
+    /**
+     * @route("create-answer/", name="apiCreateAnswer", methods="POST")
+     */
+    public function apiCreateAnswer(Request $request){
+        if ($request->getMethod() == "POST"){
+            $content = $request->request->get('content');
+            $id = $request->request->get('subjectId');
+
+            if (!preg_match('#^[a-zA-Z0-9 \'\-_ !,.?:/]{5,10000}$#', $content)){
+                $msg['content'] = true;
+            }
+            if (!preg_match('#^[0-9]+$#', $id)){
+                $msg['id'] = true;
+            }
+
+            if (!isset($msg)){
+                $repo = $this->getDoctrine()->getRepository(Subject::class);
+                $subject = $repo->findOneById($id);
+                $newAnswer = new Answer();
+                $newAnswer
+                    ->setContent($content)
+                    ->setDate(new DateTime())
+                    ->setVerified(0)
+                    ->setAuthor($this->get('session')->get('account'))
+                    ->setSubject($subject)
+                ;
+                $em = $this->getDoctrine()->getManager();
+                $em->merge($newAnswer);
+                $em->flush();
+                $msg['success'] = true;
+            }
+        }
+        return $this->json($msg);
+    }
+
+    /**
+     * @route("delete-subject", name="apiDeleteSubject", methods="POST")
+     */
+    public function apiDeleteSubject(Request $request){
+        $id = $request->request->get('subjectId');
+
+        $repo = $this->getDoctrine()->getRepository(Subject::class);
+        $subject = $repo->findOneById($id);
+        if ($subject != null){
+            $answers = $subject->getAnswers();
+            $em = $this->getDoctrine()->getManager();
+            foreach ($answers as $answer){
+                $em->remove($answer);
+            }
+            $em->remove($subject);
+            $em->flush();
+            $msg['success'] = true;
+        } else {
+            $msg['subjectDoesntExist'] = true;
+        }
+        return $this->json($msg);
+    }
+
+    /**
+     * @route("delete-answer/", name="apiDeleteAnswer", methods="POST")
+     */
+    public function apiDeleteAnswer(Request $request){
+        $id = $request->request->get('answerId');
+
+        $repo = $this->getDoctrine()->getRepository(Answer::class);
+        $answer = $repo->findOneById($id);
+        if ($answer != null){
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($answer);
+            $em->flush();
+            $msg['success'] = true;
+        } else {
+            $msg['AnswerDoesntExist'] = true;
+        }
+        return $this->json($msg);
+    }
+
+    /**
+     * @route("verified-answer/", name="apiVerifiedAnswer", methods="POST")
+     */
+    public function apiVerifiedAnswer(Request $request){
+        $idSubject = $request->request->get('subjectId');
+        $idAnswer = $request->request->get('answerId');
+
+        $repo = $this->getDoctrine()->getRepository(Subject::class);
+        $subject = $repo->findOneById($idSubject);
+        if ($subject != null){
+            $answers = $subject->getAnswers();
+            foreach ($answers as $answer){
+                if ($answer->getVerified() == 1){
+                    $msg['alreadyVerified'] = true;
+                }
+            }
+            if (!isset($msg['alreadyVerified'])){
+                $repo = $this->getDoctrine()->getRepository(Answer::class);
+                $answer = $repo->findOneById($idAnswer);
+                $answer->setVerified(1);
+                $em = $this->getDoctrine()->getManager();
+                $em->merge($answer);
+                $em->flush();
+                $msg['success'] = true;
+            }
+        } else {
+            $msg['AnswerDoesntExist'] = true;
+        }
+        return $this->json($msg);
+    }
+
+    /**
+     * @route("list-subjects/", name="apiSubjects", methods="POST")
+     */
+    public function apiSubjects(Request $request){
+        $filter = $request->request->get('filter');
+        $filters = explode('&', $filter);
+        $page = $filters[0];
+        if (isset($filters[1])){
+            $lang = $filters[1];
+        } else {
+            $lang = '';
+        }
+        if (!is_numeric($page)){
+            $msg['nan'] = true;
+        }
+        if (!isset($msg)){
+            $repo = $this->getDoctrine()->getRepository(Subject::class);
+            $subjects = $repo->findAllDateDesc(10 * (intval($page) - 1), '%'.$lang.'%');
+            foreach($subjects as $subject){
+                $newarray[] = array(
+                    'title' => $subject->getTitle(),
+                    'content' => $subject->getContent(),
+                    'id' => $subject->getId(),
+                    'cat' => explode(' ',$subject->getCategories())
+                );
+            }
+            $newarray[] = $repo->findCount(0, '%'.$lang.'%')[0][1];
+            $newarray[] = ceil($repo->findCount(0, '%'.$lang.'%')[0][1] / 10);
+            $newarray[] = $request->request->get('page');
+            return $this->json($newarray);
+        } else {
+            return $this->json($msg);
+        }
+    }
+}
---
+++ src/Controller/MainController.php
@@ -0,0 +1,158 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Subject;
+use App\Entity\Answer;
+use App\Entity\User;
+
+class MainController extends AbstractController{
+
+    /**
+     * @route("/", name="index")
+     */
+    public function index(){
+        return $this->render('index.html.twig');
+    }
+
+    /**
+     * @route("/inscription/", name="register")
+     */
+    public function register(){
+        if ($this->get('session')->has('account')){
+            throw new AccessDeniedHttpException();
+        }
+        return $this->render('register.html.twig');
+    }
+
+    /**
+     * @route("/profile/", name="profil")
+     */
+    public function profil(Request $request){
+        if (!$this->get('session')->has('account')){
+            throw new AccessDeniedHttpException();
+        }
+        if ($request->getMethod() == "POST"){
+            $typeAccepted = array('image/png', 'image/jpeg', 'image/gif');
+            $extensionPossible = array('png', 'jpeg', 'gif');
+            $img = $request->files->get('img');
+
+            if($img->getError() == 1 || $img->getError() == 2 || $img->getSize() > 1000000){
+                $msg['fileBig'] = true;
+            }
+            if($img->getError() == 3){
+                $msg['fileSend'] = true;
+            }
+            if($img->getError() == 6 || $img->getError() == 7 || $img->getError() == 8){
+                $msg['fileServer'] = true;
+            }
+            if (!isset($msg)){
+                $user = $this->get('session')->get('account');
+                if (in_array($img->getMimeType(), $typeAccepted)){
+                    $extension = $extensionPossible[array_keys($typeAccepted, $img->getMimeType())[0]];
+                    $fileName = $user->getId().'-profil.'.$extension;
+                    move_uploaded_file($img->getPathname(), '../public/img/'.$fileName);
+                    $user->setImage('img/'.$fileName);
+                    $em = $this->getDoctrine()->getManager();
+                    $em->merge($user);
+                    $em->flush();
+                    $msg['success'] = true;
+                }
+            }
+            return $this->render('profil.html.twig', array('msg' => $msg));
+        }
+        return $this->render('profil.html.twig');
+    }
+
+    /**
+     * @route("/se-connecter/", name="login")
+     */
+    public function login(){
+        if ($this->get('session')->has('account')){
+            throw new AccessDeniedHttpException();
+        }
+        return $this->render('login.html.twig');
+    }
+
+    /**
+     * @route("/liste-sujets/", name="subjects")
+     */
+    public function subjects(){
+            return $this->render('subjects.html.twig');
+    }
+
+    /**
+     * @route("/sujet/{id}", name="subject", requirements={"id" = "[\d]+"})
+     */
+    public function subject($id, Request $request){
+        $repo = $this->getDoctrine()->getRepository(Subject::class);
+        $article = $repo->findOneById($id);
+        $repo = $this->getDoctrine()->getRepository(Answer::class);
+        $answers = $repo->findBySubject($article, array("date" => "DESC"));
+        if ($article == null){
+            return $this->render('subject.html.twig', array('vide' => true));
+        }
+        return $this->render('subject.html.twig', array('article' => $article, 'answers' => $answers));
+    }
+
+    /**
+     * @route("/liste-utilisateur/{page}/", name="userList")
+     */
+    public function userList($page, Request $request){
+        $repo = $this->getDoctrine()->getRepository(User::class);
+        $users = $repo->findAll();
+        if (empty($users)){
+            return $this->render('userlist.html.twig', array('vide' => true));
+        }
+        dump($this->get('session')->get('account'));
+        return $this->render('userlist.html.twig', array('users' => $users));
+    }
+
+    /**
+     * @route("/deconnexion/", name="disconnect")
+     */
+    public function disconnect(){
+        if ($this->get('session')->has('account')){
+            $this->get('session')->remove('account');
+        } else {
+            throw new AccessDeniedHttpException();
+        }
+        return $this->render('disconnect.html.twig');
+    }
+
+    /**
+     * @route("/resultat/",  name="searchResults")
+     */
+    public function searchResult(Request $request){
+        $search = $request->request->get('search');
+        $keywords = explode(" ", $search);
+        $search = '%';
+        foreach($keywords as $keyword){
+            $search = $search.$keyword.'%';
+        }
+        $repo = $this->getDoctrine()->getRepository(Subject::class);
+        $articles = $repo->findByKeyWord($search);
+
+        foreach($articles as $article){
+            $letters = [];
+            $title = strtolower($article->getTitle());
+            foreach($keywords as $keyword){
+                if (strpos($title, $keyword) !== false){
+                    $title = str_replace($keyword, '<mark>'. $keyword .'</mark>', $title);
+                }
+            }
+            $article->setTitle($title);
+        }
+        if (empty($articles)){
+            return $this->render('results.html.twig', array('vide' => true));
+        }
+        return $this->render('results.html.twig', array('articles' => $articles));
+    }
+}
+
+// ceci est un ceci est un titre
