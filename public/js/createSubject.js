13e0afee1d664666ac390e00f1fb73bf03d834c7
Cyril CUVELIER <cyrilcuvelier@sfr.fr>
2019-01-17 13:38:21 +0100

    maj createSubject.js

--- public/js/createSubject.js
+++ public/js/createSubject.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
     var language;
     var category = [];
+    var style;
+
     $('.btn-category').click(function(e){
         e.preventDefault();
         var cat = $(this).val();
@@ -15,25 +17,46 @@ $(document).ready(function(){
             $(this).removeClass('btn-danger');
             $(this).addClass('btn-secondary');
         }
-        console.log(category);
     });
-    $('#selected-language').change(function(e){
+    $('#selected-style').change(function(){
+        style = $(this).val();
+    });
+    $('#selected-language').change(function(){
         language = $(this).val();
-        $('#simulation').removeClass();
-        $('#simulation').text('');
-        $('#simulation').addClass('language-'+ language);
-        $('#simulation').text($('#txt').val());
+        $('#simulation-code').removeClass();
+        $('#simulation-code').text('');
+        $('#simulation-code').addClass('language-'+ language);
+        $('#simulation-code').text($('#txt-code').val());
         Prism.highlightAll();
     });
-    $('#txt').keyup(function(){
-        $('#simulation').text($(this).val());
+    $('#txt-code').keyup(function(){
+        $('#simulation-code').text($(this).val());
         Prism.highlightAll();
     });
     $('#valider').click(function(){
-        $('#content').val($('#content').val()+parsebbcode($('#txt').val()));
-   
+        $('#content').val($('#content').val()+parsebbcode($('#txt-code').val()));
     });
 
+    $('#valider-style').click(function(){
+        $('#content').val($('#content').val()+parsebbstyle($('#txt-style').val()));
+    });
+    function parsebbstyle(str){
+        switch (style){
+            case 'error':
+                str = '[error]' + str + '[/error]';
+                break;
+            case 'underline':
+                str = '[underline]' + str + '[/underline]';
+                break;
+            case 'mark':
+                str = '[mark]' + str + '[/mark]';
+                break;
+            case 'overline':
+                str = '[overline]' + str + '[/overline]';
+                break;
+        }
+        return str;
+    }
     function parsebbcode(str){
         str = '[code='+language+'"]' + escapeHtml(str) + '[/code]';
         return str;
@@ -43,9 +66,7 @@ $(document).ready(function(){
         e.preventDefault();
         $('#errorTitle').html('Titre : ');
         $('#Success').html('');
-        $('#errorContent').html(`Contenu du sujet : <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#exampleModal">
-        Ajouter du code
-    </button>`);
+        $('#errorContent').html(`Contenu du sujet : <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#modalCode">Ajouter du code</button><button type="button" class="btn btn-danger ml-2 mr-2"  data-toggle="modal" data-target="#modalStyle">Texte stylisé</button>`);
         $('#errorTraitement').html('');
         $('#errorDescription').html('Description : ');
         $('#errorCategories').html('Catégories : ');
@@ -56,25 +77,30 @@ $(document).ready(function(){
             timeout: 4000,
             data: form.serialize()+'&categories='+category,
             success:function(data){
-                console.log(data);
                 if(data.success){
                     $('#form-subject').remove();
                     $('#success').html('<p class="alert alert-success">Félicitation, votre article a été posté !</p>')
                 }
                 if (data.title){
-                    $('#errorTitle').html('Titre : <span style="color:red;">Format du titre invalide</span>');
+                    $('#errorTitle').html('Titre : <span class="alert alert-danger">Format du titre invalide</span>');
                 }
                 if(data.content){
-                    $('#errorContent').html(`Contenu du sujet : <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#exampleModal">
-                    Ajouter du code
-                </button><span style="color:red;">Format du contenu invalide</span>`);
+                    $('#errorContent').html(`Contenu du sujet : <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#modalCode">Ajouter du code</button><button type="button" class="btn btn-danger ml-2 mr-2"  data-toggle="modal" data-target="#modalStyle">Texte stylisé</button><span class="alert alert-danger">Format du contenu invalide</span>`);
                 }
                 if(data.description){
-                    $('#errorDescription').html('Description : <span style="color:red;">Format de la description invalide</span>');
+                    $('#errorDescription').html('Description : <span class="alert alert-danger">Format de la description invalide</span>');
                 }
                 if(data.category){
-                    $('#errorCategories').html('Catégories : <span style="color:red;">Une ou plusieurs categorie(s) est invalide(s)</span>');
+                    $('#errorCategories').html('Catégories : <span class="alert alert-danger">Une ou plusieurs categorie(s) est invalide(s)</span>');
                 }
+            }, error:function(){
+
+            },
+            beforeSend:function(){
+                setOverlay();
+            },
+            complete:function(){
+                removeOverlay();
             }
         });
     });
